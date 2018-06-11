79b113e38512b5231431e1639d45edfe051cd169
Kishore Annam <annamkishore@gmail.com>
2018-06-12 01:08:29 +0530

    hello-redux-1, standalone redux

---
+++ hello-redux-1/hello-redux-standalone.js
@@ -0,0 +1,27 @@
+const redux = require("redux");
+
+//------------setup------i.e create reducer & store-------------------------
+
+// step #1, create reducer
+
+function counter(state = 0, action) {
+    switch(action.type) {
+        case 'INCREMENT': return state + 1;
+        case 'DECREMENT': return state - 1;
+        case 'default':   return state;
+    }
+}
+
+// step #2, create store
+
+let store = redux.createStore(counter);
+
+//-------------consume------i.e subscribe and dispatch------------------------
+
+store.subscribe( () => console.log(store.getState()) );
+
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
+
