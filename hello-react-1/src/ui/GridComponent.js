076e864db4e70b418067c6c57f8e26b98dfda95f
Kishore Annam <annamkishore@gmail.com>
2018-06-13 13:14:11 +0530

    grid with minimal data

--- hello-react-1/src/ui/GridComponent.js
+++ hello-react-1/src/ui/GridComponent.js
@@ -3,12 +3,38 @@ import { Grid } from 'react-redux-grid';
 
 import {plugins, data, columns} from './demoData';
 
-        const config = {
-            columns: columns,
-            data: data.data,
-            plugins: plugins,
-            stateKey: "grid-1"
-        };
+const config = {
+    columns: columns,
+    data: data.data,
+    plugins: plugins,
+    stateKey: "grid-1"
+};
+
+const config1 = {
+    columns: [
+        {
+          name: 'Emp Name',
+          dataIndex: 'Name1',
+        },
+        {
+          name: 'Emp Phone Number',
+          dataIndex: 'Phone Number',
+        },
+
+    ],
+    data: [
+        {
+         "Name1": "Sawyer",
+         "Phone Number": "(209) 915-9426",
+        },
+        {
+         "Name1": "Chadwick",
+         "Phone Number": "(666) 320-2563",
+        },
+    ],
+    plugins: {},
+    stateKey: "grid-1"
+};
 
 export default class GridComponent extends Component {
     constructor(props) {
@@ -18,7 +44,7 @@ export default class GridComponent extends Component {
     render() {
         console.log(config);
         return (
-            <Grid {...config}/>
+            <Grid {...config1}/>
         );
     }
 }
